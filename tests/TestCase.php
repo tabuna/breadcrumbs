bc3d9118cef6c81ce81adce6dc224991b949a148
tabuna <bliz48rus@gmail.com>
2020-04-24 23:27:21 +0300

    init

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,43 +7,10 @@ namespace Tabuna\Breadcrumbs\Tests;
 use Orchestra\Testbench\TestCase as BaseCase;
 use Tabuna\Breadcrumbs\Breadcrumbs;
 use Tabuna\Breadcrumbs\BreadcrumbsServiceProvider;
-use Tabuna\Breadcrumbs\Trail;
 
 
 class TestCase extends BaseCase
 {
-    /**
-     * Setup the test environment.
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-
-        /* Install application */
-        $this->loadLaravelMigrations();
-        $this->withFactories(__DIR__ . '/factories');
-    }
-
-    /**
-     * @param \Illuminate\Foundation\Application $app
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        $config = config();
-        $config->set('app.debug', true);
-
-        // set up database configuration
-        $config->set('database.connections.breadcrumbs', [
-            'driver'   => 'sqlite',
-            'database' => ':memory:',
-            'prefix'   => '',
-        ]);
-
-        $config->set('database.default', 'breadcrumbs');
-    }
-
-
     /**
      * @param \Illuminate\Foundation\Application $app
      *
