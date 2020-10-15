69c757b228e568c4856777116d08ff25b3203f0b
tabuna <bliz48rus@gmail.com>
2020-10-15 18:50:19 +0300

    refs #15 Removed white space

--- views/breadcrumbs.blade.php
+++ views/breadcrumbs.blade.php
@@ -1,13 +1,9 @@
 @foreach ($generate() as $crumbs)
     @if ($crumbs->url() && !$loop->last)
         <li class="{{$class}}">
-            <a href="{{ $crumbs->url() }}">
-                {{ $crumbs->title() }}
-            </a>
+            <a href="{{ $crumbs->url() }}">{{ $crumbs->title() }}</a>
         </li>
     @else
-        <li class="{{$class}} {{$active}}">
-            {{ $crumbs->title() }}
-        </li>
+        <li class="{{$class}} {{$active}}">{{ $crumbs->title() }}</li>
     @endif
 @endforeach
